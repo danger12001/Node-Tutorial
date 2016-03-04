8dfc3bf284afa06a1b6a15516cfbb299032433af
danger12001 <paul@projectcodex.co>
2016-03-04 13:05:33 +0200

    added file

--- node-tutorial/filter-numbers.js
+++ node-tutorial/filter-numbers.js
@@ -3,6 +3,13 @@ var numbers = [1, 13, 17, 4, 33, 19, 8 , 49, 45, 23,67,22,99,84, 23, 94];
 
 //find the even numbers
 var evenNumbers = [];
+  for(x= 0; x < numbers.length; x++){
+  if (numbers[x] % 2 === 0){
+    evenNumbers.push(numbers[x]);
+  }
+
+}
 
 //write the even numbers to the console
 console.log(evenNumbers);   // you are not allowed to change this line!!!
+ 
--- node-tutorial/users.js
+++ node-tutorial/users.js
@@ -1,11 +1,20 @@
 // a empty user list
 var users = [];
+var person = ["Jack", "Callum", "Neil"];
 
+person.forEach(function(x){
+  var obj = {
+    first_name: x,
+    last_name: "Bradshaw"
+  };
+  users.push(obj);
+});
+users.forEach(function(user){
+  console.log("Hi, " + user.first_name + " " + user.last_name + "!");
+});
 //create 3 users in the users array
 //using code like this: users.push({ first_name : 'Andy', last_name : 'Bradshaw' });
-
 //Create 3 users : Jack, Callum and Neil - they all should have the name Bradshaw
-
 //now loop through the users array and write the following data to the console below each other for each user
 //you can use the forEach method
 
